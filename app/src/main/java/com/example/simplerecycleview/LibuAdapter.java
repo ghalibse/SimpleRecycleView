99357d2927f59baf07013f26fb078895bc82d5a1
ghalibse <ghalibse@gmail.com>
2016-08-09 03:23:59 -0400

    After Click Listener and SnackBar

--- app/src/main/java/com/example/simplerecycleview/LibuAdapter.java
+++ app/src/main/java/com/example/simplerecycleview/LibuAdapter.java
@@ -1,9 +1,8 @@
 package com.example.simplerecycleview;
 
-import android.graphics.Color;
+
 import android.support.design.widget.Snackbar;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
